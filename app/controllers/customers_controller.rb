61438b249be815b3352720ba23edd88d81d9b38d
Nicholas Powers <njpowers7915@gmail.com>
2018-08-09 13:48:05 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,7 +8,6 @@ class CustomersController < ApplicationController
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
-    binding.pry
     redirect_to customers_path
   end
 
